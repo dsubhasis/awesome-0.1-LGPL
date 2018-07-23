e72cd58c696af349a6e9b1d97a754bfd98ae162e
Subhasis Dasgupta <sudasgupta@sdsc.edu>
2018-07-23 11:17:51 -0700

    adil parser

---
+++ awsm-neo4j-client/src/test/java/edu/sdsc/awesome/neo4j/awsmneo4jclient/AwsmNeo4jClientApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.sdsc.awesome.neo4j.awsmneo4jclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AwsmNeo4jClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
