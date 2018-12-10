be1a3791856d3efcf786d02453c5981ee6c253b7
Subhasis Dasgupta <sudasgupta@sdsc.edu>
2018-12-10 11:59:51 -0800

    Query Struct

--- awesome-query/src/main/java/com/awesome/query/PostgresClass.java
+++ awesome-query/src/main/java/com/awesome/query/PostgresClass.java
@@ -1,14 +1,10 @@
 package com.awesome.query;
 
+import edu.sdsc.awesome.data.common.util.ConnectionMap;
+
 import java.util.Map;
 
 public class PostgresClass {
-
-    private Map propertyMap;
-    private Map connectionMap;
-
-
-
 
 
 
--- awesome-query/src/main/java/com/awesome/query/PostgresClassImpl.java
+++ awesome-query/src/main/java/com/awesome/query/PostgresClassImpl.java
@@ -1,10 +1,43 @@
 package com.awesome.query;
 
+import edu.sdsc.awesome.common.connector.BaseStoreConnector;
+import edu.sdsc.awesome.connector.postgres.JDBCConnection;
 import edu.sdsc.awesome.data.common.util.ConnectionMap;
 
-public class PostgresClassImpl extends PostgresClass {
+import java.sql.SQLException;
+import java.util.Map;
 
-    ConnectionMap cn = new ConnectionMap();
+public class PostgresClassImpl  {
+
+    private Map InputMap;
+    private Map outputMap;
+    private Map query;
+
+
+    public void query(String query){
+
+        BaseStoreConnector bs = new BaseStoreConnector();
+        bs.PostgresConnector(InputMap, outputMap);
+
+        boolean flag = (boolean) outputMap.get("errorFlag");
+        if(!flag){
+
+            JDBCConnection jdb = (JDBCConnection) outputMap.get("connection");
+
+            try {
+                jdb.pgSQLQuery(query);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+
+
+
+
+    }
+
 
 
 }
