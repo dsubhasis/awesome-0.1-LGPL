e126772d16009a24c0363ff820ab971b5ca44c56
Subhasis Dasgupta <sudasgupta@sdsc.edu>
2018-12-10 19:13:52 -0800

    code for connector

---
+++ awsm-connector-postgres/src/test/java/edu/sdsc/awesome/connector/postgres/JDBCConnectionTest.java
@@ -0,0 +1,20 @@
+package edu.sdsc.awesome.connector.postgres;
+
+import junit.framework.TestCase;
+
+public class JDBCConnectionTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+
+    }
+
+    public void testPgSQLQuery() {
+
+
+    }
+
+    public void testPgSQLQueryJson() {
+    }
+}
